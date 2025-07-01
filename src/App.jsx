9cd5265b57c8c739cabd28b5fe9aad1b8027efaf
Josue Henrique <josue21henrique@gmail.com>
2025-07-01 14:25:22 -0300

    feat: inserindo versão para caminho de imagens

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
 import { GameCanvas } from './components/GameCanvas';
 
 export default function App() {
-  const version = '1.0.3';
-
-  const withVersion = (path) => `${path}?v=${version}`;
-
   const assetPaths = {
-    backgroundImg: withVersion('./background.png'),
-    playerImg01: withVersion('./player.png'),
-    generatorImg: withVersion('./generator.png'),
-    itemImg: withVersion('./queijo.png'),
-    arvoreBananaImg: withVersion('./arvoreBanana.png'),
-    bananaImg: withVersion('./banana.png'),
-    spotImage: withVersion('./spot.png'),
-    paymentBoxImage: withVersion('./paymentBox.png'),
-    moneyImg: withVersion('./money.png'),
-    clientImg: withVersion('./client.png'),
-    cashierImg: withVersion('./cashier.png'),
-    stockerImg: withVersion('./stocker.png'),
-    configButtonIcon: withVersion('./engrenagem.png'),
-    hatButtonIcon: withVersion('./hat.png'),
-    upgradeButtonIcon: withVersion('./upgrade.png'),
-    dailyButtonIcon: withVersion('./daily.png'),
-    garbageImg: withVersion('./garbage.png'),
+    backgroundImg: './background.png',
+    playerImg01: './player.png',
+    generatorImg: './generator.png',
+    itemImg: './queijo.png',
+    arvoreBananaImg: './arvoreBanana.png',
+    bananaImg: './banana.png',
+    spotImage: './spot.png',
+    paymentBoxImage: './paymentBox.png',
+    moneyImg: './money.png',
+    clientImg: './client.png',
+    cashierImg: './cashier.png',
+    stockerImg: './stocker.png',
+    configButtonIcon: './engrenagem.png',
+    hatButtonIcon: './hat.png',
+    upgradeButtonIcon: './upgrade.png',
+    dailyButtonIcon: './daily.png',
+    garbageImg: './garbage.png',
 
-    boxImg: withVersion('./box.png'),
-    bananaBoxImg: withVersion('./boxBanana.png'),
+    boxImg: './box.png',
+    bananaBoxImg: './boxBanana.png',
 
-    backgroundMusic: withVersion('./sounds/music.mp3'),
+    // NOVOS SONS:
+    backgroundMusic: './sounds/music.mp3',
+    //pickupSound: './sounds/pickup.wav',
 
-    upgradeIcon: withVersion('./upgrade-icon.png'),
-    bagIcon: withVersion('./bag-icon.png'),
-    moneyIcon: withVersion('./money-icon.png'),
+    // ... outros assets
+    upgradeIcon: './upgrade-icon.png',
+    bagIcon: './bag-icon.png',        
+    moneyIcon: './money-icon.png',
   };
 
   return <GameCanvas assetPaths={assetPaths} />;
